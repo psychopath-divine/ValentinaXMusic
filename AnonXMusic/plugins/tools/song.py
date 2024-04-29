92ff0ea2b297f09de2112aac41fa885ca7690424
DIVINE <68385116+psychopath-divine@users.noreply.github.com>
2024-04-29 21:32:12 +0530

    Update song.py
    Signed-off-by: DIVINE <68385116+psychopath-divine@users.noreply.github.com>

--- AnonXMusic/plugins/tools/song.py
+++ AnonXMusic/plugins/tools/song.py
@@ -63,6 +63,8 @@ async def song_downloader(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
+        rep = f"• uploader @{bn}"
+        host = str(info_dict["uploader"])
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
