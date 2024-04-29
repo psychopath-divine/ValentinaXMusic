a2c7dcdfad10481edaef07ce48043a32584659ea
DIVINE <68385116+psychopath-divine@users.noreply.github.com>
2024-04-29 22:03:19 +0530

    Update song.py
    Signed-off-by: DIVINE <68385116+psychopath-divine@users.noreply.github.com>

--- AnonXMusic/plugins/tools/song.py
+++ AnonXMusic/plugins/tools/song.py
@@ -31,17 +31,38 @@ SPAM_WINDOW_SECONDS = 5
 # ------------------------------------------------------------------------------- 
 
 @app.on_message(filters.command("song"))
-async def song_downloader(_, message):
+async def download_song(_, message):
     await message.delete()
+    user_id = message.from_user.id
+    current_time = time()
+    # Update the last message timestamp for the user
+    last_message_time = user_last_message_time.get(user_id, 0)
+
+    if current_time - last_message_time < SPAM_WINDOW_SECONDS:
+        # If less than the spam window time has passed since the last message
+        user_last_message_time[user_id] = current_time
+        user_command_count[user_id] = user_command_count.get(user_id, 0) + 1
+        if user_command_count[user_id] > SPAM_THRESHOLD:
+            # Block the user if they exceed the threshold
+            hu = await message.reply_text(f"**{message.from_user.mention} ᴘʟᴇᴀsᴇ ᴅᴏɴᴛ ᴅᴏ sᴘᴀᴍ, ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 5 sᴇᴄ**")
+            await asyncio.sleep(3)
+            await hu.delete()
+            return 
+    else:
+        # If more than the spam window time has passed, reset the command count and update the message timestamp
+        user_command_count[user_id] = 1
+        user_last_message_time[user_id] = current_time
+
     query = " ".join(message.command[1:])
+    print(query)
     m = await message.reply("🔎 finding song...")
     ydl_ops = {
-        'format': 'bestaudio[ext=m4a]',
-        'keepvideo': True,
-        'prefer_ffmpeg': False,
-        'geo_bypass': True,
-        'outtmpl': '%(title)s.%(ext)s',
-        'quite': True,
+        "format": "bestaudio[ext=m4a]",
+        "keepvideo": True,
+        "prefer_ffmpeg": False,
+        "geo_bypass": True,
+        "outtmpl": "%(title)s.%(ext)s",
+        "quite": True,
     }
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -53,6 +74,10 @@ async def song_downloader(_, message):
         open(thumb_name, "wb").write(thumb.content)
         duration = results[0]["duration"]
 
+        # Add these lines to define views and channel_name
+        views = results[0]["views"]
+        channel_name = results[0]["channel"]
+
     except Exception as e:
         await m.edit("❌ song not found.\n\n» Give me a valid song name !")
         print(str(e))
@@ -63,21 +88,19 @@ async def song_downloader(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"• uploader @{bn}"
-        host = str(info_dict["uploader"])
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
             secmul *= 60
         await m.edit("📤 uploading song...")
+        
         await message.reply_audio(
             audio_file,
-            caption=rep,
-            performer=host,
             thumb=thumb_name,
             parse_mode="md",
             title=title,
-            duration=dur,
+            caption=f"{title}\nRᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➪{message.from_user.mention}\nVɪᴇᴡs➪ {views}\nCʜᴀɴɴᴇʟ➪ {channel_name}",
+            duration=dur
         )
         await m.delete()
 
@@ -90,54 +113,8 @@ async def song_downloader(_, message):
     except Exception as e:
         print(e)
 
-        
-@app.on_message(filters.command("vsong"))    
-async def video_downloader(_, message):
-    await message.delete()
-    ydl_opts = {
-        "format": "best",
-        "keepvideo": True,
-        "prefer_ffmpeg": False,
-        "geo_bypass": True,
-        "outtmpl": "%(title)s.%(ext)s",
-        "quite": True,
-    }
-    query = " ".join(message.command[1:])
-    try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        title = results[0]["title"][:40]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        results[0]["duration"]
-        results[0]["url_suffix"]
-        results[0]["views"]
-        message.from_user.mention
-    except Exception as e:
-        print(e)
-    try:
-        msg = await message.reply("📥 downloading video...")
-        with YoutubeDL(ydl_opts) as ytdl:
-            ytdl_data = ytdl.extract_info(link, download=True)
-            file_name = ytdl.prepare_filename(ytdl_data)
-    except Exception as e:
-        traceback.print_exc()
-        return await msg.edit(f"🚫 error: `{e}`")
-    preview = wget.download(thumbnail)
-    await msg.edit("📤 uploading video...")
-    await message.reply_video(
-        file_name,
-        duration=int(ytdl_data["duration"]),
-        thumb=preview,
-        caption=ytdl_data["title"],
-    )
-    try:
-        os.remove(audio_file)
-        await msg.delete()
-    except Exception as e:
-        print(e)
+
+
 
 
 # ------------------------------------------------------------------------------- #
