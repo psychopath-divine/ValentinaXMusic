88080d5ae891f061ee61c9f9451ce6a6b66be8a2
DIVINE <68385116+psychopath-divine@users.noreply.github.com>
2024-04-29 20:21:15 +0530

    Update config.py
    Signed-off-by: DIVINE <68385116+psychopath-divine@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ValentineXHashTag/ValentinaXMusic",
+    "https://github.com/psychopath-divine/ValentinaXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
